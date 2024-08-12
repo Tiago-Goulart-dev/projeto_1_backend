d82eca90882b6240b8aa7d0ccea126570de9c782
Tiago-Goulart-dev <141263511+Tiago-Goulart-dev@users.noreply.github.com>
2024-08-12 11:54:06 -0300

    Upload do projeto

---
+++ src/cadastroclienteswing/CadastroClienteSwing.java
@@ -0,0 +1,16 @@
+package cadastroclienteswing;
+
+/**
+ *
+ * @author tiago
+ */
+public class CadastroClienteSwing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);    }
+    
+}
---
+++ src/dao/IClienteDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import domain.Cliente;
+
+/**
+ *
+ * @author tiago
+ */
+public interface IClienteDAO {
+    
+    public Boolean cadastrar(Cliente cliente);
+    
+    public void excluir(Long cpf);
+    
+    public void alterar(Cliente cliente);
+    
+    public Cliente consultar(Long cpf);
+    
+}
